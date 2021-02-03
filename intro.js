ed304d9047e6f50f5e413afd9010d5d23f1258fc
Joaquin <78387674+joaquintalice@users.noreply.github.com>
2021-02-03 18:44:31 -0300

    algo de js
    nada realmente

---
+++ intro.js
@@ -0,0 +1,38 @@
+// pedir datos y dar fecha de nacimiento 
+
+// declarar variables 
+//let nombre;
+//let apellido;
+//let edad;
+//let pais;
+//let departamento;
+//let ciudad;
+//let nacimiento;
+//let nacimiento2;
+// solicitar los datos 
+//nombre = prompt ("Ingresa tu nombre")
+//apellido = prompt ("Ingresa tu apellido")
+//edad = prompt ("Ingresa tu edad")
+//pais = prompt ("Ingrese su país")
+//departamento = prompt ("Ingrese su departamento")
+//ciudad = prompt ("Ingrese su ciudad")
+
+//calular el año de nacimiento 
+// NOTA: actualizar el año de ser necesario
+//nacimiento = 2021 - edad
+//nacimiento2 = nacimiento - 1
+
+
+// mostrar los resultados 
+// usar varios "//document.write" solo para practicar :-)
+
+//document.write ("Hola" + " " + nombre + " " + apellido + " " + "encantado de conocerte." + " ")
+//document.write ("si tu edad es" + " " + edad + " " + "puedo deducir que naciste en" + " " + nacimiento + " ")
+//document.write ("a menos que no hayas cumplido años este año. De ser ese el caso, tu fecha de nacimiento es en" + " " + nacimiento2 + " ")
+//document.write ("Es de nuestro agrado saber que tenemos visitas de" + " " + pais + ", " + departamento + ", " + ciudad  + ", " + "espero te sientas bienvenido en nuestro sitio web." + " ")
+//document.write ("Ponte cómodo/a" + " " + nombre) 
+
+let usuarios;
+
+usuarios = ["Vegetta777", "armando", "pepito", "jaime", "agustin", "jessica"]
+document.write ("La cantidad de usuarios conectados ahora mismo es de" + " " + usuarios.length + ". " + "Muchas gracias por formar parte de nuestra pequeña comunidad.")
